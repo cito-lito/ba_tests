bcb2dcf24a207d8177d441a6ad0230b3ece64727
mmo <mario.a.orozco@student.hs-rm.de>
2022-02-27 20:27:06 +0100

    deploy to rinkeby

--- test001/scripts/deploy.py
+++ test001/scripts/deploy.py
@@ -1,22 +1,28 @@
-from brownie import accounts, Owner
+from brownie import accounts, network, config,  Owner
 
 ## add account: brownie accounts new nameOfneAccount
 ### accounts.load("my_account")
 
 def deploy_owner():
-    #use ganache chain:
-    owner_contract = Owner.deploy({"from":accounts[1]})
+    #
+    account = get_account()
+    owner_contract = Owner.deploy({"from":account})
     print(f"owner_contract is {owner_contract}")
     owner_acc = owner_contract.getOwner()
     print(f"owner is {owner_acc}")
     
     # change owner:
-    tx = owner_contract.changeOwner("0x1667E22c87d39Ad1c9fC5D22123C42b7bcB2fC3d", {"from":accounts[1]})
+    #tx = owner_contract.changeOwner("0x1667E22c87d39Ad1c9fC5D22123C42b7bcB2fC3d", {"from":account})
     #tx.wait(1)
-    owner_acc = owner_contract.getOwner()
-    print(f"owner is {owner_acc}")
+   # owner_acc = owner_contract.getOwner()
+   # print(f"owner is {owner_acc}")
 
 
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.load("dev")
 
 def main():
     deploy_owner()
