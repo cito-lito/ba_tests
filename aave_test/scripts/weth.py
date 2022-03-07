4fc7b452010dee2c6097ee5b4093e7e18e46bc3b
mmo <thetechpomelo@gmail.com>
2022-03-07 17:03:17 +0100

    weth deposit withdraw

---
+++ aave_test/scripts/weth.py
@@ -0,0 +1,25 @@
+from brownie import accounts, interface, config, network, Wei
+
+
+    
+def deposit(account, amount):
+    weth = interface.WethInterface(config["networks"][network.show_active()]["weth_token"])
+    tx = weth.deposit({"from":account, "value":amount})   
+    tx.wait(1)
+    return tx 
+    
+def witdraw(account, amount):
+    weth = interface.WethInterface(config["networks"][network.show_active()]["weth_token"])
+    tx = weth.withdraw(amount, {"from":account})    
+    tx.wait(1)
+    return tx 
+    
+
+def main():
+    ## checking...
+    account = accounts.load("dev")
+    tx = deposit(account, Wei("0.1 ether"))  
+    print(f"deposit transaction is: {tx}")
+    tx = witdraw(account, Wei("0.05 ether"))  
+    print(f"witdraw transaction is: {tx}")
+    
