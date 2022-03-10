5963780092f84596adc7e36b068d78e63964c122
mmo <thetechpomelo@gmail.com>
2022-03-10 19:54:28 +0100

    get pool contract

--- aave_test/scripts/aave.py
+++ aave_test/scripts/aave.py
@@ -1,15 +1,19 @@
 from scripts.utils import get_account
 from scripts import weth
-from brownie import Wei
+from brownie import Wei, interface, network, config
 
 
-
-def get_lending_pool():
-    pass
+def get_lending_pool_contract():
+    #account = get_account("dev")
+    lending_pool_addr_prov = interface.ILendingPoolAddressesProvider(
+        config["networks"][network.show_active()]["aave_pool_addr_provider"])
+    print(f"lending pool addr: {lending_pool_addr_prov}")
+    lending_pool_contract = lending_pool_addr_prov.getLendingPool()
+    print(f"lending pool contract: {lending_pool_contract}")
+    return lending_pool_contract
 
 
 
 def main():
-    account = get_account()
-    tx = weth.deposit(account, Wei("0.2 ether"))
-    print(tx)
+    get_lending_pool_contract()
+    
